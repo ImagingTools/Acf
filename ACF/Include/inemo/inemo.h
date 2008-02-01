a0df4edca5406abc5b2dd55c89d555fc01fce44b
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2008-02-01 15:15:41 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@7 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ ACF/Include/inemo/inemo.h
@@ -0,0 +1,15 @@
+#ifndef inemo_h_included
+#define inemo_h_included
+
+/**
+\defgroup inemo
+@{
+*/
+
+/** 
+@} 
+*/
+
+
+
+#endif // !inemo_h_included
---
+++ ACF/Include/iqtnemo/iqtnemo.h
@@ -0,0 +1,15 @@
+#ifndef iqtnemo_h_included
+#define iqtnemo_h_included
+
+/**
+\defgroup iqtnemo
+@{
+*/
+
+/** 
+@} 
+*/
+
+
+
+#endif // !iqtnemo_h_included
