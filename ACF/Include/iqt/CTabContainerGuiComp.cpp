4118e089f8675304541b355cc2e75b70180f0a43
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2008-02-02 19:23:01 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@19 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ ACF/Include/iqt/CTabContainerGuiComp.cpp
@@ -0,0 +1,52 @@
+#include <QGridLayout>
+
+#include "QtAcf/QtAcf.h"
+
+
+#include "iqtstd/CTabContainerGuiComp.h"
+
+
+namespace iqtstd
+{
+
+
+CTabContainerGuiComp::CTabContainerGuiComp()
+	:BaseClass(), 
+	m_slaveGuisCompIfPtr(this, "SlaveGuiComponents")
+{
+
+}
+
+
+CTabContainerGuiComp::~CTabContainerGuiComp()
+{
+
+}
+
+
+// reimplemented (acf::QtAbstractGuiComponent)
+
+void CTabContainerGuiComp::initializeGui()
+{
+	if (m_widget != NULL){
+		int tabCount = m_slaveGuisCompIfPtr.dependencyCount();
+		for (int tabIndex = 0; tabIndex < tabCount; tabIndex++){
+			acf::QtGuiInterface* guiPtr = m_slaveGuisCompIfPtr.interfacePtr(tabIndex);
+			if (guiPtr != NULL){
+				QWidget* tab = new QWidget(m_widget);
+
+				guiPtr->attachTo(tab);
+			}
+		}
+	}
+}
+
+
+void CTabContainerGuiComp::destroyGui()
+{
+
+}
+
+
+
+} // namespace iqtstd
