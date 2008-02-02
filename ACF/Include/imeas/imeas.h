380d59aaa9713d79725f9fdc57cae7c5a84c7f0c
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2008-02-02 01:14:00 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@17 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ ACF/Include/imeas/imeas.h
@@ -0,0 +1,26 @@
+#ifndef imeas_h_included
+#define imeas_h_included
+
+
+/**
+\defgroup imeas
+\ingroup ACF
+@{
+*/
+
+/** 
+@} 
+*/
+
+
+/**
+	\namespace imeas
+	\brief This namespace contains classes for measurement system implementations
+*/
+namespace imeas
+{
+	
+} // namespace imeas
+
+
+#endif // !imeas_h_included
