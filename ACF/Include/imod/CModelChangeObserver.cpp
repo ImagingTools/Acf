8e4db75118833236e9118848d1b6fb0be9f77cea
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2008-02-28 17:30:13 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@102 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ ACF/Include/imod/CModelChangeObserver.cpp
@@ -0,0 +1,49 @@
+#include "imod/CModelChangeObserver.h"
+
+
+namespace imod
+{
+
+
+// public methods
+
+CModelChangeObserver::CModelChangeObserver()
+	:m_isDirty(false),
+	m_modelObserver(*this)
+{
+}
+
+
+CModelChangeObserver::CModelChangeObserver(imod::IModel* modelPtr)
+	:m_isDirty(false),
+	m_modelObserver(*this)
+{
+	SetModelPtr(modelPtr);
+}
+
+
+void CModelChangeObserver::SetModelPtr(imod::IModel* modelPtr)
+{
+	if (modelPtr != NULL){
+		modelPtr->AttachObserver(&m_modelObserver);
+	}
+}
+
+
+
+// protected methods
+
+bool CModelChangeObserver::IsDirty() const
+{
+	return m_isDirty;
+}
+
+
+void CModelChangeObserver::SetDirty(bool isDirty)
+{
+	m_isDirty = isDirty;
+}
+
+
+} // namespace imod
+
---
+++ ACF/Include/imod/CModelChangeObserver.h
@@ -0,0 +1,63 @@
+#ifndef imod_CModelChangeObserver_included
+#define imod_CModelChangeObserver_included
+
+
+#include "imod/imod.h"
+
+#include "imod/TSingleModelObserverBase.h"
+
+
+namespace imod
+{
+
+
+/**
+	\ingroup istd
+
+	This class catchs the updating methods of a given model and marks it as dirty.
+	To clear this flag you must call SetDirty(false) manually.
+*/
+class CModelChangeObserver: virtual public istd::IPolymorphic
+{
+public:
+	friend class Observer;
+
+	CModelChangeObserver();
+	CModelChangeObserver(imod::IModel* modelPtr);
+
+	virtual void SetModelPtr(imod::IModel* modelPtr);
+
+protected:
+	virtual void SetDirty(bool isDirty = true);
+	virtual bool IsDirty() const;
+
+	class Observer: public imod::TSingleModelObserverBase<imod::IModel>
+	{
+	public:
+		Observer(CModelChangeObserver& parent)
+			:m_parent(parent)
+		{
+		}
+
+		// reimplemented (TSingleModelObserverBase)
+		virtual void OnUpdate(imod::IModel* modelPtr, int updateFlags = 0, istd::IPolymorphic* updateParamsPtr = NULL)
+		{
+			m_parent.SetDirty(true);
+		}
+
+		CModelChangeObserver& m_parent;
+	};
+
+private:
+	bool m_isDirty;
+
+	Observer m_modelObserver;
+};
+
+
+} // namespace imod
+
+
+#endif // !imod_CModelChangeObserver_included
+
+
