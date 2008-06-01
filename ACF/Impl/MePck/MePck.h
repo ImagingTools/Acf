1b8fc542d05ed00896d94d3601a359bf5661a053
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2008-06-01 23:31:00 +0000

    Meilhaus package added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@367 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ ACF/Impl/MePck/MePck.h
@@ -0,0 +1,25 @@
+#ifndef MePck_included
+#define MePck_included
+
+
+#include "icomp/TModelCompWrap.h"
+#include "icomp/TMakeComponentWrap.h"
+
+
+
+/**
+	Meilhaus-specific package.
+*/
+namespace MePck
+{
+
+
+//typedef iqt::CLoginGuiComp LoginGui;
+
+
+} // namespace MePck
+
+
+#endif // !MePck_included
+
+
