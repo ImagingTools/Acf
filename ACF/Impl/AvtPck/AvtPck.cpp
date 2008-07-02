1a6614e111d774b1c0d2681453140527c610c81b
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2008-07-02 14:50:05 +0000

    some small changes.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@411 5144ca80-2444-0410-90ef-8152f95a0219

--- ACF/Impl/AvtPck/AvtPck.cpp
+++ ACF/Impl/AvtPck/AvtPck.cpp
@@ -9,7 +9,7 @@ namespace AvtPck
 
 I_EXPORT_PACKAGE("AvtPck", "Standard Applied Vision Technologies (AVT) hardware package based on Qt", "AVT Applied Vision Technologies Qt Standard");
 
-I_EXPORT_COMPONENT(FireGrabAcquisition, "Implementation of camera interface using FireGrab API", "AVT Grabber Camera Acquisiotion Bitmap Image FireGrab");
+I_EXPORT_COMPONENT(FireGrabAcquisition, "Implementation of camera interface using FireGrab API", "AVT Grabber Camera Acquisition Bitmap Image FireGrab");
 
 
 } // namespace AvtPck
