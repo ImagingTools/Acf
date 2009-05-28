54fffee6a6b6e64c7659b35ee4267d74b777bb42
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2009-05-28 13:45:06 +0000

    Generated Compositor (maxi) added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@930 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Impl/CompositorExe/main.cpp
@@ -0,0 +1,28 @@
+// Qt includes
+#include <QCoreApplication>
+
+// ACF includes
+#include "ibase/IApplication.h"
+#include "iqt/CDefaultServicesProvider.h"
+
+#include "Generated/CCompositor.h"
+
+
+int main(int argc, char *argv[])
+{
+	Q_INIT_RESOURCE(iqtgui);
+	Q_INIT_RESOURCE(icmpstr);
+
+	iqt::CDefaultServicesProvider::RegisterServices();
+
+	CCompositor instance;
+
+	ibase::IApplication* applicationPtr = instance.GetInterface<ibase::IApplication>();
+
+	if (applicationPtr != NULL){
+		return applicationPtr->Execute(argc, argv);
+	}
+
+	return -1;
+}
+
