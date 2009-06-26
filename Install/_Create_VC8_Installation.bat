b28f4b88e01c5be7b2e3706d2c519ce28462e7ab
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2009-06-26 14:28:09 +0000

    Version 0.9.1 Installation scripts corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@966 5144ca80-2444-0410-90ef-8152f95a0219

--- Install/_Create_VC8_Installation.bat
+++ Install/_Create_VC8_Installation.bat
@@ -1,5 +1,15 @@
 echo off
 
+if not exist Temp goto DelTempSkip
+echo Removeing old temporary files...
+rmdir /Q /S Temp
+:DelTempSkip
+
+if not exist TempExcl goto DelTempExclSkip
+echo Removeing old temporary scripts...
+rmdir /Q /S TempExcl
+:DelTempExclSkip
+
 call Rebuild_VC8.bat
 
 call Prepare_ACF.bat
@@ -7,4 +17,13 @@ call Prepare_ACF.bat
 call Create_TechnicalDoc.bat
 
 call Create_VC_LGPL.bat
+
+echo Createing installation file...
+iscc.exe /Q TempExcl\acf.iss
+
+echo Installation done
+
+echo Removeing temporary files...
+rmdir /Q /S Temp
+rmdir /Q /S TempExcl
 
