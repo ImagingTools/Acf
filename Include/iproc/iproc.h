25877784ce0492a7347214a5b98281ada58a6a93
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2009-11-12 12:50:24 +0000

    Big cleaning action: some stuff moved from IACF, some stuff moved to ACF Solutions.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1165 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/iproc/iproc.h
@@ -0,0 +1,25 @@
+#ifndef iproc_included
+#define iproc_included
+
+
+#include "ibase/ibase.h"
+
+
+/**
+	This namespace containes interfaces and implementation of data processing concepts.
+	This package is system independent.
+	This package use following ACF packages:
+	\li	istd
+	\li	iser
+	\li	ibase
+	\li	iprm
+*/
+namespace iproc
+{
+
+
+} // namespace iproc
+
+
+#endif // !iproc_included
+
