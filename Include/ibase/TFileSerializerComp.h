88a622d09a5a0cd9a4fd5a72efde8b1126dff70a
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2010-05-03 12:26:05 +0000

    Deprecated file type info implementation removed from TFileSerializerComp.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1488 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/ibase/TFileSerializerComp.h
+++ Include/ibase/TFileSerializerComp.h
@@ -40,8 +40,6 @@ public:
 		I_REGISTER_INTERFACE(iser::IFileTypeInfo);
 		I_REGISTER_INTERFACE(iser::IFileLoader);
 		I_ASSIGN(m_versionInfoCompPtr, "VersionInfo", "Provide information about archive versions", false, "VersionInfo");
-		I_ASSIGN_MULTI_0(m_fileExtensionsAttrPtr, "FileExtensions", "List of supported file extensions", false);
-		I_ASSIGN_MULTI_0(m_typeDescriptionsAttrPtr, "TypeDescriptions", "List of descriptions for each extension", false);
 	I_END_COMPONENT;
 
 	// reimplemented (iser::IFileLoader)
@@ -52,10 +50,6 @@ public:
 				bool beQuiet = true) const;
 	virtual int LoadFromFile(istd::IChangeable& data, const istd::CString& filePath = istd::CString()) const;
 	virtual int SaveToFile(const istd::IChangeable& data, const istd::CString& filePath = istd::CString()) const;
-
-	// reimplemented (iser::IFileTypeInfo)
-	virtual bool GetFileExtensions(istd::CStringList& result, int flags = 0, bool doAppend = false) const;
-	virtual istd::CString GetTypeDescription(const istd::CString* extensionPtr = NULL) const;
 
 protected:
 	class ReadArchiveEx: public ReadArchive
@@ -145,8 +139,6 @@ protected:
 
 private:
 	I_REF(iser::IVersionInfo, m_versionInfoCompPtr);
-	I_MULTIATTR(istd::CString, m_fileExtensionsAttrPtr);
-	I_MULTIATTR(istd::CString, m_typeDescriptionsAttrPtr);
 };
 
 
@@ -183,10 +175,11 @@ bool TFileSerializerComp<ReadArchive, WriteArchive>::IsOperationSupported(
 			}
 		}
 
-		if (m_fileExtensionsAttrPtr.IsValid()){
-			int extensionsCount = m_fileExtensionsAttrPtr.GetCount();
+		istd::CStringList fileExtensions;
+		if (GetFileExtensions(fileExtensions)){
+			int extensionsCount = fileExtensions.size();
 			for (int i = 0; i < extensionsCount; ++i){
-				const istd::CString& extension = m_fileExtensionsAttrPtr[i];
+				const istd::CString& extension = fileExtensions[i];
 				if (filePathPtr->substr(filePathPtr->length() - extension.length() - 1) == istd::CString(".") + extension.ToLower()){
 					return true;
 				}
@@ -254,51 +247,6 @@ int TFileSerializerComp<ReadArchive, WriteArchive>::SaveToFile(const istd::IChan
 	}
 
 	return StateFailed;
-}
-
-
-// reimplemented (iser::IFileTypeInfo)
-
-template <class ReadArchive, class WriteArchive>
-bool TFileSerializerComp<ReadArchive, WriteArchive>::GetFileExtensions(istd::CStringList& result, int /*flags*/, bool doAppend) const
-{
-	if (m_fileExtensionsAttrPtr.IsValid()){
-		if (!doAppend){
-			result.clear();
-		}
-
-		int extensionsCount = m_fileExtensionsAttrPtr.GetCount();
-		for (int i = 0; i < extensionsCount; ++i){
-			const istd::CString& extension = m_fileExtensionsAttrPtr[i];
-
-			result.push_back(extension);
-		}
-
-		return true;
-	}
-	else{
-		return false;
-	}
-}
-
-
-template <class ReadArchive, class WriteArchive>
-istd::CString TFileSerializerComp<ReadArchive, WriteArchive>::GetTypeDescription(const istd::CString* extensionPtr) const
-{
-	if (extensionPtr != NULL){
-		int extensionsCount = istd::Min(m_fileExtensionsAttrPtr.GetCount(), m_typeDescriptionsAttrPtr.GetCount());
-		for (int extIndex = 0; extIndex < extensionsCount; extIndex++){
-			if (m_fileExtensionsAttrPtr[extIndex] == *extensionPtr){
-				return m_typeDescriptionsAttrPtr[extIndex];
-			}
-		}
-	}
-
-	if (m_typeDescriptionsAttrPtr.GetCount() > 0){
-		return m_typeDescriptionsAttrPtr[0];
-	}
-
-	return "";
 }
 
 
