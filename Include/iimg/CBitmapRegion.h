a64a76e653cc8406f1de95e924db5ae8cdd3022d
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2011-06-21 17:25:45 +0000

    Bitmap region implementation added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1790 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/iimg/CBitmapRegion.h
@@ -0,0 +1,77 @@
+#ifndef iimg_CBitmapRegion_included
+#define iimg_CBitmapRegion_included
+
+
+// STL includes
+#include <vector>
+
+
+// ACF includes
+#include "i2d/CRectangle.h"
+#include "i2d/CCircle.h"
+#include "i2d/CAnnulus.h"
+
+#include "iimg/IBitmap.h"
+
+
+namespace iimg
+{
+
+
+/**
+	Representation of a 2D-region as container of bitmap line scans.
+*/
+class CBitmapRegion: virtual public istd::IPolymorphic
+{
+public:
+	struct PixelRange
+	{
+		PixelRange()
+			:pixelBufferPtr(NULL)
+		{
+		}
+
+		istd::CRange range;
+		const void* pixelBufferPtr;
+	};
+
+	typedef std::vector<PixelRange> PixelRanges;
+
+	CBitmapRegion(const IBitmap* bitmapPtr = NULL);
+
+	/**
+		Create 2D-region from the geometrical object.
+	*/
+	bool CreateFromGeometry(const i2d::IObject2d& geometry);
+
+	/**
+		Get the list of pixel ranges per given line.
+	*/
+	const PixelRanges* GetPixelRanges(int lineIndex) const;
+
+	const i2d::CRectangle& GetBoundingBox() const;
+	void ResetBitmapRegion();
+	bool IsBitmapRegionEmpty() const;
+
+private:
+	void CalculateRegionBoundingBox(const i2d::CRectangle& objectBoundingBox);
+	void CreateFromCircle(const i2d::CCircle& circle);
+	void CreateFromRectangle(const i2d::CRectangle& rect);
+	void CreateFromAnnulus(const i2d::CAnnulus& annulus);
+
+private:
+	const IBitmap* m_bitmapPtr;
+	::std::vector<PixelRanges> m_rangesContainer;
+	::std::vector<const PixelRanges*> m_lineRangePtr;
+	i2d::CRectangle m_boundingBox;
+	bool m_isEmpty;
+};
+
+
+} // namespace iimg
+
+
+#endif // !iimg_CBitmapRegion_included
+
+
+
