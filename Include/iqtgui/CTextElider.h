3c26e1e22da04664e2ebcc5d4a89eb11ad776b0e
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2011-08-25 08:41:02 +0000

    Text eliding helper class added. Small cosmetic corrections.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1841 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/iqtgui/CTextElider.h
@@ -0,0 +1,67 @@
+#ifndef iqtgui_CTextElider_included
+#define iqtgui_CTextElider_included
+
+
+// QT includes
+#include <QObject>
+#include <QString>
+#include <QMap>
+#include <QTimer>
+
+
+namespace iqtgui
+{
+
+
+/*
+	Helper class to control visual text behavoir during resizing of widgets, which contain text representation
+	(e.g. QAbstractButton, QLineEdit, etc.).
+	If the widget size is smaller than the size of the text, the text will be croped and shown with a specified suffix.
+	For example the text "blablabla" will be shown as "blabl...".
+	You can register multiple widget objects by the text elider with the method RegisterElideObject().
+	If you want specifiy your own elide suffix, use SetElideSuffix() method.
+*/
+class CTextElider: public QObject
+{
+public:
+	CTextElider(QObject* parentPtr = NULL);
+	virtual ~CTextElider();
+
+	bool RegisterElideObject(QObject* elideObject);
+	void UnregisterElideObject(QObject* elideObject);
+	void UnregisterAllElideObjects();
+	void SetUpdateInterval(int updateInterval = 300);
+	void SetElideSuffix(QString elideSuffix);
+	QString GetElideSuffix() const;
+
+protected:
+	// reimplemented (QObject)
+	virtual bool eventFilter(QObject* objectPtr, QEvent* eventPtr);
+
+private:
+	void UpdateElidedText(QObject& elideObject);
+
+	void OnTimeout();
+
+private:
+	struct ElideObjectInfo
+	{
+		QString baseString;
+		QString elideString;
+	};
+
+	typedef QMap<QObject*, ElideObjectInfo> ElideObjectsMap;
+	ElideObjectsMap m_objectPtrMap;
+
+	QTimer m_textObservingTimer;
+	int m_updateInterval;
+	QString m_elideSuffix;
+};
+
+
+} // namespace iqtgui
+
+
+#endif // !iqtgui_CTextElider_included
+
+
