d125180d025c14586139984def8edd3c33eea6a6
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2011-09-03 12:21:07 +0000

    Issue #BUILD-1: Updating of ACF version moved to SVN client side using pre-commit hook mechanism in Tortoise SVN. Please see Issue BUILD-1 to get help for your tortoise client configuration.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1871 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Config/Subversion/UpdateAcfVersion.cmd
@@ -0,0 +1,2 @@
+SubWCRev %ACFDIR% %ACFDIR%\Include\istd\AcfVersion.h.xtrsvn %ACFDIR%\Include\istd\AcfVersion.h
+SubWCRev %ACFDIR% %ACFDIR%\Partitura\AcfVoce.arp\VersionInfo.arx.xtrsvn %ACFDIR%\Partitura\AcfVoce.arp\VersionInfo.arx
