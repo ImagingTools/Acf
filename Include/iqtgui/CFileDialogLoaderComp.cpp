d3a3763d6edcbeb41fa01d8167b48c395c8ebef0
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2011-09-11 07:06:10 +0000

    File dialog loader corrected. UI starting mode attribute in StandaloneApp exported.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1903 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtgui/CFileDialogLoaderComp.cpp
+++ Include/iqtgui/CFileDialogLoaderComp.cpp
@@ -136,7 +136,9 @@ int CFileDialogLoaderComp::AppendLoaderFilterList(const iser::IFileTypeInfo& fil
 	int retVal = 0;
 
 	istd::CStringList docExtensions;
-	fileTypeInfo.GetFileExtensions(docExtensions, flags);
+	if (!fileTypeInfo.GetFileExtensions(docExtensions, flags)){
+		return 0;
+	}
 
 	istd::CString commonDescription = fileTypeInfo.GetTypeDescription();
 
@@ -236,7 +238,7 @@ QString CFileDialogLoaderComp::GetFileName(const istd::CString& filePath, bool i
 			retVal = QFileDialog::getSaveFileName(
 						NULL,
 						tr("Enter file name"), 
-						m_lastSaveInfo.absolutePath(),
+						m_lastSaveInfo.absoluteFilePath(),
 						filterList,
 						&selectedFilter); 
 		}
@@ -244,7 +246,7 @@ QString CFileDialogLoaderComp::GetFileName(const istd::CString& filePath, bool i
 			retVal = QFileDialog::getOpenFileName(
 						NULL,
 						tr("Select a file to open"),
-						m_lastOpenInfo.absolutePath(),
+						m_lastOpenInfo.absoluteFilePath(),
 						filterList,
 						&selectedFilter);
 		}
@@ -254,16 +256,18 @@ QString CFileDialogLoaderComp::GetFileName(const istd::CString& filePath, bool i
 		selectionIndex = filterList.count('\n');
 	}
 
-	QFileInfo fileInfo(retVal);
-	if (isSaving){
-		m_lastSaveInfo = fileInfo;
-	}
-	else{
-		m_lastOpenInfo = fileInfo;
-	}
+	if (!retVal.isEmpty()){
+		QFileInfo fileInfo(retVal);
+		if (isSaving){
+			m_lastSaveInfo = fileInfo;
+		}
+		else{
+			m_lastOpenInfo = fileInfo;
+		}
 
-	if (m_statupDirectoryCompPtr.IsValid()){
-		m_statupDirectoryCompPtr->SetPath(iqt::GetCString(fileInfo.dir().absolutePath()));
+		if (m_statupDirectoryCompPtr.IsValid()){
+			m_statupDirectoryCompPtr->SetPath(iqt::GetCString(fileInfo.dir().absolutePath()));
+		}
 	}
 
 	return retVal;
