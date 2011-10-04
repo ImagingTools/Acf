566895b7a173e870b63ce464e5945ef9356128af
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2011-10-04 10:26:05 +0000

    Concept of model dispatcher redesigned.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1938 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/imod/CMultiModelDispatcherBase.cpp
@@ -0,0 +1,66 @@
+#include "imod/CMultiModelDispatcherBase.h"
+
+
+// ACF includes
+#include "imod/IModel.h"
+
+
+namespace imod
+{
+
+
+bool CMultiModelDispatcherBase::RegisterModel(IModel* modelPtr, int modelId, int relevantFlags)
+{
+	ObserverProxyPtr& proxyPtr = m_modelMap[modelId];
+	if (!proxyPtr.IsValid()){
+		proxyPtr.SetPtr(new ObserverProxy(this, modelId, relevantFlags));
+	}
+
+	if (!modelPtr->AttachObserver(proxyPtr.GetPtr())){
+		m_modelMap.erase(modelId);
+
+		return false;
+	}
+
+	return true;
+}
+
+
+void CMultiModelDispatcherBase::UnregisterModel(int modelId)
+{
+	m_modelMap.erase(modelId);
+}
+
+
+void CMultiModelDispatcherBase::UnregisterAllModels()
+{
+	m_modelMap.clear();
+}
+
+
+// protected methods of the embedded class ObserverProxy
+
+CMultiModelDispatcherBase::ObserverProxy::ObserverProxy(CMultiModelDispatcherBase* parentPtr, int modelId, int relevantFlags)
+:	m_parent(*parentPtr),
+	m_modelId(modelId),
+	m_relevantFlags(relevantFlags)
+{
+	I_ASSERT(parentPtr != NULL);
+}
+
+
+// reimplemented (imod::CMultiModelObserverBase)
+
+void CMultiModelDispatcherBase::ObserverProxy::OnUpdate(int changeFlags, istd::IPolymorphic* updateParamsPtr)
+{
+	if ((m_relevantFlags != 0) && (m_relevantFlags & changeFlags) == 0){
+		return;
+	}
+		
+	m_parent.OnModelChanged(m_modelId, changeFlags, updateParamsPtr);
+}
+
+
+} // namespace imod
+
+
---
+++ Include/imod/CMultiModelDispatcherBase.h
@@ -0,0 +1,102 @@
+#ifndef imod_CMultiModelDispatcherBase_included
+#define imod_CMultiModelDispatcherBase_included
+
+
+// STL includes
+#include <map>
+
+// ACF includes
+#include "istd/TDelPtr.h"
+#include "imod/IModel.h"
+#include "imod/CSingleModelObserverBase.h"
+
+
+namespace imod
+{
+
+
+/**
+	Generic implementation of a data model changes notifier.
+
+	You can register several data models objects that you want to observe with RegisterModel method.
+	You will get a notification event on each change in the observed data model via OnModelChanged method call of your notification consumer.  
+	\note Notification consumer class must implement the method
+	void OnModelChanged(int modelId, int changeFlags, istd::IPolymorphic* updateParamsPtr) to support the notification callback.
+*/
+class CMultiModelDispatcherBase: virtual public istd::IPolymorphic
+{
+public:
+	/**
+		Register the data model to be observed. If model registration was successfull, the function returns \c true.
+		\param modelPtr	Pointer to the data model object, which should be observed.
+		\param modelId	Logical model ID for possible event filtering in the notification callback.
+		\param relefantFlags	The notification will only be triggered if the value of \c relevantFlags matches the model change flags.
+	*/
+	bool RegisterModel(IModel* modelPtr, int modelId = 0, int relevantFlags = 0);
+	
+	/**
+		Unregister the data model object.
+		\param modelId	object ID used at the object registration.
+		\sa RegisterModel
+	*/
+	void UnregisterModel(int modelId = 0);
+
+	/**
+		Unregister all observed data objects.
+	*/
+	void UnregisterAllModels();
+
+	/**
+		Get concrete object instance of the obsered model.
+	*/
+	template <class Object>
+	Object* GetObjectAt(int modelId) const;
+
+protected:
+	class ObserverProxy: public CSingleModelObserverBase
+	{
+	public:
+		typedef CSingleModelObserverBase BaseClass;
+
+		ObserverProxy(CMultiModelDispatcherBase* parentPtr, int modelId, int relevantFlags);
+
+		// reimplemented (imod::CSingleModelObserverBase)
+		virtual void OnUpdate(int changeFlags, istd::IPolymorphic* updateParamsPtr);
+
+	private:
+		CMultiModelDispatcherBase& m_parent;
+		int m_modelId;
+		int m_relevantFlags;
+	};
+
+	// abstract methods
+	virtual void OnModelChanged(int modelId, int changeFlags, istd::IPolymorphic* updateParamsPtr) = 0;
+
+private:
+	typedef istd::TDelPtr<ObserverProxy> ObserverProxyPtr;
+	typedef std::map<int, ObserverProxyPtr> ModelMap;
+
+	ModelMap m_modelMap;
+};
+
+
+// public template methods
+
+template <class Object>
+Object* CMultiModelDispatcherBase::GetObjectAt(int modelId) const
+{
+	typename ModelMap::const_iterator foundIter = m_modelMap.find(modelId);
+	if ((foundIter != m_modelMap.end()) && foundIter->second.IsValid()){
+		return dynamic_cast<Object*>(foundIter->second->GetModelPtr());
+	}
+
+	return NULL;
+}
+
+
+} // namespace imod
+
+
+#endif // !imod_CMultiModelDispatcherBase_included
+
+
