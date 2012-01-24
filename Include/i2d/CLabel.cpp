ea447098e862cd1cad0ea3f901b5ab1f09c9a8ff
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2012-01-24 23:07:12 +0000

    New 2D primitives added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2147 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/i2d/CLabel.cpp
@@ -0,0 +1,40 @@
+#include "i2d/CLabel.h"
+
+
+namespace i2d
+{
+
+
+CLabel::CLabel(const i2d::CVector2d& position, const istd::CString& labelText)
+	:BaseClass(position), 
+	m_text(labelText)
+{
+}
+
+
+void CLabel::SetText(const istd::CString& labelText)
+{
+	m_text = labelText;
+}
+
+
+// reimplemented (iser::ISerializable)
+
+bool CLabel::Serialize(iser::IArchive& archive)
+{
+	bool retVal = true;
+
+	retVal = retVal && BaseClass::Serialize(archive);
+
+	static iser::CArchiveTag textTag("Text", "Text of label");
+	retVal = retVal && archive.BeginTag(textTag);
+	retVal = retVal && archive.Process(m_text);
+	retVal = retVal && archive.EndTag(textTag);
+
+	return retVal;
+}
+
+
+} // namespace i2d
+
+
