dbab7ce984982f6d0d98f130d26a5040cafcb55f
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2012-01-25 08:36:39 +0000

    Unimportant.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2148 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iproc/TSyncProcessorCompBase.h
+++ Include/iproc/TSyncProcessorCompBase.h
@@ -35,7 +35,7 @@ public:
 
 // global typedefs
 
-typedef TSyncProcessorCompBase<iproc::IProcessor> CSyncProcessorCompBase;
+typedef TSyncProcessorCompBase<IProcessor> CSyncProcessorCompBase;
 
 
 } // namespace iproc
