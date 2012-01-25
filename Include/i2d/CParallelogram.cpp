21f3fbac8724325875f11e16d31a778e699ed14f
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2012-01-25 16:21:30 +0000

    CParallelogram added. Range implementation made generic.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2149 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/i2d/CParallelogram.cpp
@@ -0,0 +1,101 @@
+#include "i2d/CParallelogram.h"
+
+
+
+namespace i2d
+{
+
+
+// reimplemented (i2d::IObject2d)
+
+i2d::CVector2d CParallelogram::GetCenter() const
+{
+	return m_transform.GetTranslation();
+}
+
+
+void CParallelogram::MoveCenterTo(const i2d::CVector2d& position)
+{
+	m_transform.SetTranslation(position);
+}
+
+
+bool CParallelogram::Transform(
+			const i2d::ITransformation2d& transformation,
+			i2d::ITransformation2d::ExactnessMode mode,
+			double* /*errorFactorPtr*/)
+{
+	i2d::CAffine2d localTransform;
+	if (transformation.GetLocalTransform(GetCenter(), localTransform, mode)){
+		m_transform.Apply(localTransform);
+
+		return true;
+	}
+
+	return true;
+}
+
+
+bool CParallelogram::InvTransform(
+			const i2d::ITransformation2d& transformation,
+			i2d::ITransformation2d::ExactnessMode mode,
+			double* /*errorFactorPtr*/)
+{
+	i2d::CAffine2d localTransform;
+	if (transformation.GetLocalInvTransform(GetCenter(), localTransform, mode)){
+		m_transform.Apply(localTransform);
+
+		return true;
+	}
+
+	return true;
+}
+
+
+bool CParallelogram::GetTransformed(
+			const i2d::ITransformation2d& transformation,
+			i2d::IObject2d& result,
+			i2d::ITransformation2d::ExactnessMode mode,
+			double* errorFactorPtr) const
+{
+	CParallelogram* parallelogramPtr = dynamic_cast<CParallelogram*>(&result);
+	if (parallelogramPtr != NULL){
+		return parallelogramPtr->Transform(transformation, mode, errorFactorPtr);
+	}
+
+	return false;
+}
+
+
+bool CParallelogram::GetInvTransformed(
+			const i2d::ITransformation2d& transformation,
+			i2d::IObject2d& result,
+			i2d::ITransformation2d::ExactnessMode mode,
+			double* errorFactorPtr) const
+{
+	CParallelogram* parallelogramPtr = dynamic_cast<CParallelogram*>(&result);
+	if (parallelogramPtr != NULL){
+		return parallelogramPtr->InvTransform(transformation, mode, errorFactorPtr);
+	}
+
+	return false;
+}
+
+
+// reimplemented (iser::ISerializable)
+
+bool CParallelogram::Serialize(iser::IArchive& archive)
+{
+	bool retVal = true;
+
+	static iser::CArchiveTag transformTag("Transform", "Transformation used in parallelogram");
+	retVal = retVal && archive.BeginTag(transformTag);
+	retVal = retVal && m_transform.Serialize(archive);
+	retVal = retVal && archive.EndTag(transformTag);
+
+	return retVal;
+}
+
+
+} // namespace i2d
+
---
+++ Include/istd/CIntRange.h
@@ -0,0 +1,22 @@
+#ifndef istd_CIntRange_included
+#define istd_CIntRange_included
+
+
+#include "istd/TRange.h"
+
+
+namespace istd
+{
+
+
+/**
+	Range of two integer numbers.
+*/
+typedef istd::TRange<int> CIntRange;
+
+
+} // namespace istd
+
+
+#endif // !istd_CIntRange_included
+
