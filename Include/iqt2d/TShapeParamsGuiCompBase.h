57a49e67f96f70f2512a91da83eedab5b7c80037
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2012-03-07 09:54:16 +0000

    Cosmetic changes.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2220 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqt2d/TShapeParamsGuiCompBase.h
+++ Include/iqt2d/TShapeParamsGuiCompBase.h
@@ -24,7 +24,6 @@ public:
 
 	I_BEGIN_COMPONENT(TShapeParamsGuiCompBase);
 		I_ASSIGN(m_unitNameAttrPtr, "UnitName", "Name of geometric units e.g. mm", false, "mm");
-		I_ASSIGN(m_zValueAttrPtr, "ZValue", "Describe draw priority on display console (the objects with bigger value will overlap the other ones)", true, 1);
 	I_END_COMPONENT;
 
 	// reimplemented (imod::IObserver)
@@ -41,7 +40,6 @@ protected:
 	virtual void CreateShapes(int sceneId, Shapes& result);
 
 	I_ATTR(QString, m_unitNameAttrPtr);
-	I_ATTR(double, m_zValueAttrPtr);
 };
 
 
@@ -136,7 +134,6 @@ void TShapeParamsGuiCompBase<Ui, Shape, ShapeModel>::UpdateShapeView()
 template <class Ui, class Shape, class ShapeModel>
 void TShapeParamsGuiCompBase<Ui, Shape, ShapeModel>::CreateShapes(int /*sceneId*/, Shapes& result)
 {
-	I_ASSERT(m_zValueAttrPtr.IsValid());	// this attribute is obligatory
 	Shape* shapePtr = new Shape();
 	if (shapePtr != NULL){
 		result.PushBack(shapePtr);
--- Include/iview/CAffiniteCalibration.h
+++ Include/iview/CAffiniteCalibration.h
@@ -97,3 +97,5 @@ private:
 
 
 #endif // !iview_CAffiniteCalibration_included
+
+
