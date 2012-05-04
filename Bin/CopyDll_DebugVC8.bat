25f489ce96bdfa2cde02e8b830c5b219ac1a344d
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2012-05-04 15:57:42 +0000

    Scripts for DLL copy corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2307 5144ca80-2444-0410-90ef-8152f95a0219

--- Bin/CopyDll_DebugVC8.bat
+++
@@ -1,4 +0,0 @@
-copy /Y %QTDIR%\bin\*.dll DebugVC8
-copy /Y %QTDIR%\bin\*.dll.Manifest DebugVC8
-xcopy /Y %QTDIR%\plugins\imageformats\*.* DebugVC8\imageformats\*.*
-xcopy /Y %QTDIR%\plugins\iconengines\*.* DebugVC8\iconengines\*.*
