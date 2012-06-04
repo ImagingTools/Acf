1e3a3a7a020d954404c6c5c45df8b715b11e47ac
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2012-06-04 16:18:30 +0000

    Update Tortoise version info moved from 'Config' to 'Build'.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2359 5144ca80-2444-0410-90ef-8152f95a0219

--- Build/VC9/BuildAll.bat
+++ Build/VC9/BuildAll.bat
@@ -2,7 +2,7 @@ echo off
 
 call "%VS90COMNTOOLS%vsvars32.bat" x86
 
-call ..\..\Config\Tortoise\UpdateVersion.cmd
+call ..\Tortoise\UpdateVersion.cmd
 
 msbuild AcfAll.sln
 msbuild AcfAll.sln /p:Configuration=Release
