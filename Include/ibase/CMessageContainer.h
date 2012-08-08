44028a98a8eb19d0f1aeb2b6a026256d6315c6d5
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2012-08-08 13:11:16 +0000

    ibase::TMessageContainerWrap renamed and simplified to ibase::CMessageContainer.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2428 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/ibase/CMessageContainer.h
@@ -0,0 +1,70 @@
+#ifndef ibase_CMessageContainer_included
+#define ibase_CMessageContainer_included
+
+
+// Qt includes
+#include <QtCore/QList>
+
+// ACF includes
+#include "istd/IInformationProvider.h"
+
+#include "ibase/IMessageContainer.h"
+#include "ibase/THierarchicalBase.h"
+
+
+namespace ibase
+{		
+
+
+/**
+	Basic implementation of a message container.
+*/
+class CMessageContainer: public ibase::THierarchicalBase<IHierarchicalMessageContainer>
+{
+public:
+	CMessageContainer();
+	virtual ~CMessageContainer();
+
+	virtual void AddChildContainer(IHierarchicalMessageContainer* childContainerPtr);
+
+	// reimplemented (iser::ISerializable)
+	virtual bool Serialize(iser::IArchive& archive);
+
+	// reimplemented (ibase::IMessageContainer)
+	virtual void SetMaxMessageCount(int maxMessageCount = -1);
+	virtual void SetMaxLiveTime(int maxLiveTime = -1);
+	virtual int GetWorstCategory() const;
+	virtual Messages GetMessages() const;
+	virtual void ClearMessages();
+
+	// reimplemented (ibase::IMessageConsumer)
+	virtual bool IsMessageSupported(
+				int messageCategory = -1,
+				int messageId = -1,
+				const istd::IInformationProvider* messagePtr = NULL) const;
+	virtual void AddMessage(const IMessageConsumer::MessagePtr& messagePtr);
+
+	// reimplemented (ibase::IHierarchicalMessageContainer)
+	virtual int GetChildsCount() const;
+	virtual IMessageContainer* GetChild(int index) const;
+
+private:
+	typedef QList<IMessageContainer::MessagePtr> MessageList;
+	MessageList m_messages;
+
+	typedef QVector<IHierarchicalMessageContainer*> Childs;
+	Childs m_childContainers;
+
+	int m_maxMessageCount;
+	int m_maxLiveTime;
+
+	int m_worstCategory;
+};
+
+
+} // namespace ibase
+
+
+#endif // !ibase_CMessageContainer_included
+
+
