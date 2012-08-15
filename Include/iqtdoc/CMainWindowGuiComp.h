cda90d0a819c8bd36e61bc7371025fb1f84b144c
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2012-08-15 10:56:44 +0000

    Using of QApplication::arguments in CMainWindowGuiComp replaced by IApplication::GetCommandLine
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2449 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtdoc/CMainWindowGuiComp.h
+++ Include/iqtdoc/CMainWindowGuiComp.h
@@ -10,6 +10,7 @@
 #include "imod/IModel.h"
 #include "imod/TSingleModelObserverBase.h"
 
+#include "ibase/IApplication.h"
 #include "ibase/IApplicationInfo.h"
 #include "ibase/ICommandsProvider.h"
 
@@ -41,6 +42,7 @@ public:
 		I_ASSIGN(m_documentManagerCompPtr, "DocumentManager", "Document manager", true, "DocumentManager");
 		I_ASSIGN_TO(m_documentManagerModelCompPtr, m_documentManagerCompPtr, true);
 		I_ASSIGN_TO(m_documentManagerCommandsCompPtr, m_documentManagerCompPtr, false);
+		I_ASSIGN(m_applicationCompPtr, "Application", "Access to the application's command line", true, "Application");
 		I_ASSIGN(m_isCopyPathVisibleAttrPtr, "IsCopyPathVisible", "If true, operation Tools/CopyDocumentPath will be visible", true, false);
 		I_ASSIGN(m_isOpenContainingFolderVisibleAttrPtr, "IsOpenContainingFolderVisible", "If true, operation Tools/Open Containing Folder will be visible", true, false);
 		I_ASSIGN(m_maxRecentFilesCountAttrPtr, "MaxRecentFiles", "Maximal size of recent file list for one document type", true, 10);
@@ -211,6 +213,7 @@ private:
 	I_REF(idoc::IDocumentManager, m_documentManagerCompPtr);
 	I_REF(imod::IModel, m_documentManagerModelCompPtr);
 	I_REF(ibase::ICommandsProvider, m_documentManagerCommandsCompPtr);
+	I_REF(ibase::IApplication, m_applicationCompPtr);
 	I_ATTR(bool, m_isCopyPathVisibleAttrPtr);
 	I_ATTR(bool, m_isOpenContainingFolderVisibleAttrPtr);
 	I_ATTR(int, m_maxRecentFilesCountAttrPtr);
