d01c5b6ba77417fa46ab3356069d529ba7d4d3cc
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2012-09-10 17:20:51 +0000

    Bug in TRange fixed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2511 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/istd/TRange.h
+++ Include/istd/TRange.h
@@ -398,7 +398,7 @@ inline bool TRange<ValueType>::Contains(ValueType value) const
 {
 	I_ASSERT(IsValid());
 
-	return (value  >= m_minValue) && (value < m_maxValue);
+	return (value  >= m_minValue) && (value <= m_maxValue);
 }
 
 
