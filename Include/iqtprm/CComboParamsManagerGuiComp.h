b8fe0d148390f0b2239ab414ab18e9a046e9bccc
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2012-10-11 08:37:04 +0000

    Cosmetics.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2595 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtprm/CComboParamsManagerGuiComp.h
+++ Include/iqtprm/CComboParamsManagerGuiComp.h
@@ -14,6 +14,9 @@ namespace iqtprm
 {
 
 
+/**
+	Simple parameter manager editor using a combo box to parameter selection.
+*/
 class CComboParamsManagerGuiComp: public iqtgui::TDesignerGuiObserverCompBase<
 			Ui::CComboParamsManagerGuiComp,
 			iprm::IParamsManager>
