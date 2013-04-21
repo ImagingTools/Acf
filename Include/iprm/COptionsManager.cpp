6f5b9c90e65158bba2ae6ff16050876b718b4ca4
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2013-04-21 19:04:44 +0000

    Option manager extended
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2908 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iprm/COptionsManager.cpp
+++ Include/iprm/COptionsManager.cpp
@@ -130,6 +130,14 @@ bool COptionsManager::IsOptionEnabled(int index) const
 }
 
 
+// reimplemented (iprm::IOptionsManager)
+
+int COptionsManager::GetIndexOperationFlags(int /*index*/) const
+{
+	return OOF_SUPPORT_INSERT | OOF_SUPPORT_DELETE;
+}
+
+
 bool COptionsManager::SetOptionEnabled(int index, bool isEnabled)
 {
 	Q_ASSERT(index >= 0);
@@ -155,8 +163,6 @@ bool COptionsManager::SetOptionEnabled(int index, bool isEnabled)
 	return true;
 }
 
-
-// reimplemented (iprm::IOptionsManager)
 
 bool COptionsManager::RemoveOption(int index)
 {
