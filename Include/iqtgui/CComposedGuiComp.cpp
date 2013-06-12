f72624dc2ed0b3244dc0bb6c5e2453332a2455e9
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2013-06-12 15:47:07 +0000

    Stretching policy added to the CComposedGuiComp.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2985 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtgui/CComposedGuiComp.cpp
+++ Include/iqtgui/CComposedGuiComp.cpp
@@ -183,6 +183,11 @@ void CComposedGuiComp::OnGuiCreated()
 
 				if (parentLayoutPtr != NULL){
 					parentLayoutPtr->addWidget(elementParentPtr);
+
+					Q_ASSERT(layoutPtr != NULL);
+					if (m_useSameStretchingFactorAttrPtr.IsValid() && *m_useSameStretchingFactorAttrPtr){
+						layoutPtr->setStretchFactor(elementParentPtr, 1);
+					}
 				}
 
 				if (isFlat){
