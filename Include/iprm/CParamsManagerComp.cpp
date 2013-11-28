6b4f5979e61186995188d52799e93333561828de
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2013-11-28 22:55:38 +0000

    Crash in CParamsManagerComp fixed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3254 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iprm/CParamsManagerComp.cpp
+++ Include/iprm/CParamsManagerComp.cpp
@@ -56,7 +56,7 @@ bool CParamsManagerComp::SetSetsCount(int count)
 					paramsModelPtr->AttachObserver(this);
 				}
 
-				m_paramSets[i].TakeOver(paramsSetPtr);
+				m_paramSets[i - fixedSetsCount].TakeOver(paramsSetPtr);
 			}
 		}
 	}
