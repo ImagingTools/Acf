52ea72fc5e6c6c1c5cb1ef76ce144c57d210b421
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2014-03-17 19:22:09 +0000

    Change notification for interactive shapes corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3392 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iview/CPolylineShape.cpp
+++ Include/iview/CPolylineShape.cpp
@@ -79,8 +79,6 @@ bool CPolylineShape::OnMouseButton(istd::CIndex2d position, Qt::MouseButton butt
 						if (tickerBox.IsInside(position - spLast)){
 							BeginModelChanges();
 
-							istd::CChangeNotifier notifier(polylinePtr);
-
 							if (polylinePtr->InsertNode(cpLast)){
 								m_referencePosition = cpLast - GetLogPosition(position);
 								m_referenceIndex = nodesCount;
@@ -95,8 +93,6 @@ bool CPolylineShape::OnMouseButton(istd::CIndex2d position, Qt::MouseButton butt
 							istd::CIndex2d sp = GetScreenPosition(middle).ToIndex2d();
 							if (tickerBox.IsInside(position - sp)){
 								BeginModelChanges();
-
-								istd::CChangeNotifier notifier(polylinePtr);
 
 								if (polylinePtr->InsertNode(i + 1, middle)){
 									m_referencePosition = middle - GetLogPosition(position);
@@ -113,8 +109,6 @@ bool CPolylineShape::OnMouseButton(istd::CIndex2d position, Qt::MouseButton butt
 							istd::CIndex2d spFirst = GetScreenPosition(cpFirst).ToIndex2d();
 							if (tickerBox.IsInside(position - spFirst)){
 								BeginModelChanges();
-
-								istd::CChangeNotifier notifier(polylinePtr);
 
 								if (polylinePtr->InsertNode(0, cpFirst)){
 									m_referencePosition = cpFirst - GetLogPosition(position);
@@ -138,8 +132,6 @@ bool CPolylineShape::OnMouseButton(istd::CIndex2d position, Qt::MouseButton butt
 							istd::CIndex2d sp = GetScreenPosition(cp).ToIndex2d();
 							if (tickerBox.IsInside(position - sp)){
 								BeginModelChanges();
-
-								istd::CChangeNotifier notifier(polylinePtr);
 
 								polylinePtr->RemoveNode(i);
 
