8adaeda02f42717f4ad4a5583e3b53432e6b716b
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2015-04-01 12:28:06 +0000

    Some warnings for CLANG fixed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3692 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iser/CArchiveTag.cpp
+++ Include/iser/CArchiveTag.cpp
@@ -6,10 +6,10 @@ namespace iser
 
 
 CArchiveTag::CArchiveTag()
-:	m_parentPtr(NULL),
- 	m_tagType(TT_UNKNOWN),
- 	m_isTagSkippingUsed(false),
- 	m_binaryId(0)
+:	m_binaryId(0),
+	m_tagType(TT_UNKNOWN),
+	m_parentPtr(NULL),
+	m_isTagSkippingUsed(false)
 {
 }
 
@@ -21,12 +21,12 @@ CArchiveTag::CArchiveTag(
 			CArchiveTag* parentPtr,
 			bool isTagSkippingUsed)
 :	m_id(id),
+	m_binaryId(0),
 	m_comment(comment),
 	m_tagType(tagType),
 	m_parentPtr(parentPtr),
 	m_isTagSkippingUsed(isTagSkippingUsed)
 {
-	m_binaryId = 0;
 	int idSize = int(m_id.size());
 
 	for (int i = 0; i < idSize; ++i){
