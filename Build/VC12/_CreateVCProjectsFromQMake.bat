1162fe69dc05d9cb8ffa57c6df6150dd7e454ff5
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2015-08-05 08:53:58 +0000

    Projects creation for VC12 added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3795 5144ca80-2444-0410-90ef-8152f95a0219

--- Build/VC12/_CreateVCProjectsFromQMake.bat
+++ Build/VC12/_CreateVCProjectsFromQMake.bat
@@ -1,13 +1,13 @@
 rem echo off
 
-set COMPILER_EXT=VC10
-set QMAKESPEC=%QTDIR%/mkspecs/win32-msvc2010
+set COMPILER_EXT=VC12
+set QMAKESPEC=%QTDIR%/mkspecs/win32-msvc2013
 
 cd ..\..
 
 echo Generating %COMPILER_EXT% projects...
 cd Build/QMake
-qmake -recursive -tp vc AcfAll.pro
+%QTDIR%\bin\qmake -recursive -tp vc AcfAll.pro
 cd ../..
 
 call %ACFDIR%/Config/QMake/CopyVCProjToSubdir.js %COMPILER_EXT%
