d27e8dfb57bb043e4436f550c7dbb3336ad9f7ff
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2015-09-28 10:21:30 +0000

    Creation of public library added. Public library contains binaries and headers but not contains sources.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3911 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Install/_Create_VC12_Public.bat
@@ -0,0 +1,30 @@
+echo off
+
+set LICENSE_TYPE=Public
+set LICENSE_DIR=Install/LGPL
+set LICENSE_INSTALL_PATH=../LGPL/License.txt
+set ACF_CONFIG_FILE=$(ACFSLNDIR)/Config/Core.xpc
+
+call %~dp0\..\Build\VC12\_CreateVCProjectsFromQMake.bat
+
+cd %~dp0\..
+
+set COMPILER_EXT=VC12
+call Install\InternalParts\CreateTempDirs.bat
+
+call Install\InternalParts\CopyDlls.bat
+
+echo Compiling...
+msbuild %~dp0\..\Build\%COMPILER_EXT%\AcfAll.sln /p:Configuration=Debug
+msbuild %~dp0\..\Docs\Tutorial\Build\%COMPILER_EXT%\TutorialAcf.sln /p:Configuration=Debug
+msbuild %~dp0\..\Build\%COMPILER_EXT%\AcfAll.sln /p:Configuration=Release
+msbuild %~dp0\..\Docs\Tutorial\Build\%COMPILER_EXT%\TutorialAcf.sln /p:Configuration=Release
+echo Compiling done
+
+call Install\InternalParts\CreateScripts.bat
+
+call Install\InternalParts\Create_TechnicalDoc.bat
+
+call Install\InternalParts\CopyTempFiles.bat
+
+echo Installation done
