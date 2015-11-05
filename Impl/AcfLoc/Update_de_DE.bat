0c6ee6fa6f5de35f6f82626de4ca2ef060a3e07d
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2015-11-05 13:39:38 +0000

    Switch to another concept of translation deployment.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4017 5144ca80-2444-0410-90ef-8152f95a0219

--- Impl/AcfLoc/Update_de_DE.bat
+++
@@ -1,1 +0,0 @@
-%QTDIR%\bin\lupdate.exe ..\..\ -target-language de_DE -ts Acf_de_DE.ts -recursive -locations relative
--- Impl/AcfLoc/Update_ru_RU.bat
+++
@@ -1,1 +0,0 @@
-%QTDIR%\bin\lupdate.exe ..\..\ -target-language ru_RU -ts Acf_ru_RU.ts -recursive -locations relative
