9cce45ada6631c0a560887e5e82927f2cbb9f960
am <am@5144ca80-2444-0410-90ef-8152f95a0219>
2016-05-03 09:49:42 +0000

    potential crash fixed
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4168 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtgui/CSplashScreenGuiComp.cpp
+++ Include/iqtgui/CSplashScreenGuiComp.cpp
@@ -1,3 +1,25 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org or write info@imagingtools.de for further
+** 	information about the ACF.
+**
+********************************************************************************/
+
+
 #include "iqtgui/CSplashScreenGuiComp.h"
 
 
@@ -50,6 +72,8 @@ void CSplashScreenGuiComp::AddMessage(const MessagePtr& messagePtr)
 
 void CSplashScreenGuiComp::OnGuiCreated()
 {
+	BaseClass::OnGuiCreated();
+
 	Q_ASSERT(IsGuiCreated());
 	QSplashScreen* splashScreenPtr = GetQtWidget();
 	Q_ASSERT(splashScreenPtr != NULL);
@@ -197,6 +221,9 @@ void CSplashScreenGuiComp::OnGuiRetranslate()
 
 void CSplashScreenGuiComp::OnAddMessage(const MessagePtr& messagePtr)
 {
+	if (!IsGuiCreated())
+		return;
+
 	QString text = messagePtr->GetInformationDescription();
 	if (text.isEmpty()){
 		ProgressLabel->clear();
