1e9014c9755b2f6fdb983971ca1717f400e8241f
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2016-09-12 11:50:42 +0000

    Crash in CExtComposedParamsSetGuiComp fixed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4288 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtprm/CExtComposedParamsSetGuiComp.cpp
+++ Include/iqtprm/CExtComposedParamsSetGuiComp.cpp
@@ -295,9 +295,7 @@ void CExtComposedParamsSetGuiComp::OnGuiModelDetached()
 
 	iqt::CSignalBlocker blocker(GetPageContainerWidget());
 
-	for (int pageIndex = 0; pageIndex < GetPagesCount(); ++pageIndex){
-		RemovePage(pageIndex);
-	}
+	ResetPages();
 
 	m_connectedEditorsMap.clear();
 
