83ea1d4e8d1f51b559e0f38706b9133523ee3963
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2016-11-28 15:10:03 +0000

    File name parameter editor: The order of file filters.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4363 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtprm/CFileNameParamGuiComp.cpp
+++ Include/iqtprm/CFileNameParamGuiComp.cpp
@@ -194,7 +194,7 @@ void CFileNameParamGuiComp::on_BrowseButton_clicked()
 							filterList);
 
 				if (allExt.size() > 1){
-					filterList += tr("All supported files (%1)").arg("*." + allExt.join(" *."));
+					filterList.push_front(tr("All supported files (%1)").arg("*." + allExt.join(" *.")));
 				}
 			}
 
