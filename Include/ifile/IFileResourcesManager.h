0f9f1468bedfacb009f0e8ed909f6da8e80b05ea
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2017-11-25 17:33:08 +0000

    Interface corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4607 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/ifile/IFileResourcesManager.h
+++ Include/ifile/IFileResourcesManager.h
@@ -10,6 +10,7 @@
 #include <ilog/IMessageConsumer.h>
 #include <iprm/IOptionsList.h>
 #include <idoc/IDocumentMetaInfo.h>
+#include <ifile/IFileMetaInfoProvider.h>
 
 
 namespace ifile
@@ -98,7 +99,7 @@ public:
 		\param resourceId			ID of the file resource.
 		\return Pointer to the meta-info instance or \c NULL, if no information could be provided.
 	*/
-	virtual const idoc::IDocumentMetaInfo* GetFileMetaInfo(const QByteArray& resourceId) const = 0;
+	virtual ifile::IFileMetaInfoProvider::ConstMetaInfoPtr GetFileMetaInfo(const QByteArray& resourceId) const = 0;
 
 	/**
 		Get the name of the file resource in the storage.
