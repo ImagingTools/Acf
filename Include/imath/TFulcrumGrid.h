e90e4015fc1e0cf10cc8614d1ec9199a42deee39
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2018-03-23 15:43:32 +0000

    imath::TFulcrumGrid: management of layers for fixed size grids was corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4665 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/imath/TFulcrumGrid.h
+++ Include/imath/TFulcrumGrid.h
@@ -46,6 +46,8 @@ public:
 		CF_SORT_LAYERS = 0x8352707
 	};
 
+	TFulcrumGrid();
+
 	/**
 		Removes all fulcrum points.
 	*/
@@ -198,6 +200,13 @@ inline double TFulcrumGrid<Position, Fulcrums>::GetLayerPosition(int dimension, 
 // public methods
 
 template <class Position, class Fulcrums>
+TFulcrumGrid<Position, Fulcrums>::TFulcrumGrid()
+{
+	m_layers.resize(m_fulcrums.GetDimensionsCount());
+}
+
+
+template <class Position, class Fulcrums>
 void TFulcrumGrid<Position, Fulcrums>::Reset()
 {
 	m_fulcrums.Reset();
@@ -205,6 +214,8 @@ void TFulcrumGrid<Position, Fulcrums>::Reset()
 	for (Layers::iterator iter = m_layers.begin(); iter != m_layers.end(); ++iter){
 		iter->clear();
 	}
+
+	m_layers.resize(m_fulcrums.GetDimensionsCount());
 }
 
 
