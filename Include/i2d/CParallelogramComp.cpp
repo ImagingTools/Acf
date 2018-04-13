f54d635735056576636525bad199d66ac5b4926d
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2018-04-13 12:45:10 +0000

    CParallelogramComp: Extensions (NKA, AM)
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4674 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/i2d/CParallelogramComp.cpp
@@ -0,0 +1,29 @@
+#include <i2d/CParallelogramComp.h>
+
+
+namespace i2d
+{
+
+
+// protected methods
+
+// reimplemented (icomp::CComponentBase)
+
+void CParallelogramComp::OnComponentCreated()
+{
+	BaseClass::OnComponentCreated();
+
+	Q_ASSERT(m_xAttrPtr.IsValid());
+	Q_ASSERT(m_yAttrPtr.IsValid());
+	Q_ASSERT(m_widthAttrPtr.IsValid());
+	Q_ASSERT(m_heightAttrPtr.IsValid());
+
+	SetTranslation(i2d::CVector2d(*m_xAttrPtr, *m_yAttrPtr));
+	SetWidth(*m_widthAttrPtr);
+	SetHeight(*m_heightAttrPtr);
+}
+
+
+} // namespace qrtv
+
+
