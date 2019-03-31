56fa2f5698fbca887ee6ad10c70d089ac50fedb5
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2019-03-31 11:10:11 +0000

    CMultiPageGuiCompBase: Crash fixed CExtComposedParamsSetGuiComp: Crash fixed
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4771 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtprm/CExtComposedParamsSetGuiComp.cpp
+++ Include/iqtprm/CExtComposedParamsSetGuiComp.cpp
@@ -282,10 +282,6 @@ void CExtComposedParamsSetGuiComp::OnGuiModelDetached()
 
 	iqt::CSignalBlocker blocker(GetPageContainerWidget());
 
-	ResetPages();
-
-	m_connectedEditorsMap.clear();
-
 	int elementsCount = qMin(m_observersCompPtr.GetCount(), m_idsAttrPtr.GetCount());
 	for (int i = 0; i < elementsCount; ++i){
 		const QByteArray& paramId = m_idsAttrPtr[i];
@@ -303,6 +299,10 @@ void CExtComposedParamsSetGuiComp::OnGuiModelDetached()
 			}
 		}
 	}
+
+	m_connectedEditorsMap.clear();
+
+	ResetPages();
 
 	BaseClass::OnGuiModelDetached();
 }
