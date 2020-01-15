ad4a73b50ad831ca02ec3fbe0ad6c0c91e8f70e1
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2020-01-15 14:35:16 +0000

    Experimental unit test integration added. Some cosmetic corrections
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4829 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/itest/CStandardTestExecutor.h
@@ -0,0 +1,59 @@
+#ifndef itest_CStandardTestExecutor_included
+#define itest_CStandardTestExecutor_included
+
+
+// Qt includes
+#include <QtCore/QObject>
+#include <QtCore/QVector>
+
+// ACF includes
+#include <istd/TSmartPtr.h>
+#include <itest/ITestExecutor.h>
+
+
+namespace itest
+{
+
+
+/**
+	Standard implementation of ITestExecutor with static test registration
+*/
+class CStandardTestExecutor: virtual public ITestExecutor
+{
+public:
+	// reimplemented (ITestExecutor)
+	virtual int RunTests(int argc, char *argv[]);
+
+public:
+	template <class TestObject>
+	class TestRegistrator
+	{
+	public:
+		TestRegistrator(const QString& name)
+		{
+			TestPtr testPtr(new TestObject);
+
+			testPtr->setObjectName(name);
+
+			AddTest(testPtr);
+		}
+	};
+
+private:
+	typedef istd::TSmartPtr<QObject> TestPtr;
+	typedef QVector<TestPtr> TestInstances;
+
+	static TestInstances& GetTests();
+	static bool FindTest(const QString& name);
+	static void AddTest(TestPtr test);
+};
+
+
+} // namespace itest
+
+#define I_ADD_TEST(className) const itest::CStandardTestExecutor::TestRegistrator<className> className##_AddTest(#className);
+
+
+#endif // !itest_CStandardTestExecutor_included
+
+
---
+++ Include/itest/ITestExecutor.h
@@ -0,0 +1,25 @@
+#ifndef itest_ITestContainer_included
+#define itest_ITestContainer_included
+
+// ACF includes
+#include <istd/IPolymorphic.h>
+
+
+namespace itest
+{
+
+
+/**
+	Interface for running a collection of tests
+*/
+class ITestExecutor: virtual public istd::IPolymorphic
+{
+public:
+	virtual int RunTests(int argc, char *argv[]) = 0;
+};
+
+
+} // namespace itest
+
+
+#endif // !itest_ITestContainer_included
