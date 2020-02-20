729c057c27ea967e5b6110e304de1b9218f318e0
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2020-02-20 09:44:49 +0000

    Comment added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4845 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/imod/CMultiModelDispatcherBase.h
+++ Include/imod/CMultiModelDispatcherBase.h
@@ -75,6 +75,9 @@ protected:
 	};
 
 	// abstract methods
+	/**
+		Called when some model observed by dispatcher will be changed.
+	*/
 	virtual void OnModelChanged(int modelId, const istd::IChangeable::ChangeSet& changeSet) = 0;
 
 private:
