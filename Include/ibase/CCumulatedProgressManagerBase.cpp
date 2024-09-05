4225c1cb47d8274e3ca881cb2ad8515c1d1aef54
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2024-09-05 14:54:05 +0000

    Progress corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@5236 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/ibase/CCumulatedProgressManagerBase.cpp
+++ Include/ibase/CCumulatedProgressManagerBase.cpp
@@ -204,9 +204,10 @@ void CCumulatedProgressManagerBase::CloseTask(TaskBase* taskPtr)
 		if (taskIter->second.isCancelable) {
 			m_cancelableCounter--;
 		}
-		m_openTasks.erase(taskPtr);
 
-		m_closedTaskSum += 1;
+		m_closedTaskSum += taskIter->second.weight;
+
+		m_openTasks.erase(taskPtr);
 	}
 
 	locker.unlock();
@@ -237,11 +238,11 @@ void CCumulatedProgressManagerBase::TryUpdateCumulatedProgress()
 
 	QMutexLocker locker(&m_tasksMutex);
 	if (m_maxProgressSum > 0) {
-		double progressSum = m_closedTaskSum;
+		progressValue = m_closedTaskSum;
 
 		for (const auto& task : m_openTasks) {
 			const auto& taskInfo = task.second;
-			progressSum += taskInfo.progress;
+			progressValue += (taskInfo.progress * taskInfo.weight);
 		}
 	}
 	locker.unlock();
