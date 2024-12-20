5f8c65dd816027ffa09914881926ab9ae141f856
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2024-12-20 09:39:35 +0000

    Simple encryption persistence
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@5330 5144ca80-2444-0410-90ef-8152f95a0219

--- Impl/FilePck/FilePck.h
+++ Impl/FilePck/FilePck.h
@@ -36,6 +36,8 @@
 #include <ifile/CJsonDeviceReadArchive.h>
 #include <ifile/CJsonDeviceWriteArchive.h>
 #include <ifile/TDeviceBasedSerializerComp.h>
+#include <ifile/CFileReadSecureArchive.h>
+#include <ifile/CFileWriteSecureArchive.h>
 
 
 /**
@@ -68,6 +70,7 @@ typedef ifile::CTempFileManagerComp TempFileManager;
 typedef ifile::TFileSerializerComp<ifile::CJsonFileReadArchive, ifile::CJsonFileWriteArchive> JsonFileSerializer;
 typedef ifile::TDeviceBasedSerializerComp<ifile::CCompactXmlDeviceReadArchive, ifile::CCompactXmlDeviceWriteArchive> CompactXmlDeviceSerializer;
 typedef ifile::TDeviceBasedSerializerComp<ifile::CJsonDeviceReadArchive, ifile::CJsonDeviceWriteArchive> JsonDeviceSerializer;
+typedef ifile::TFileSerializerComp<ifile::CFileReadSecureArchive, ifile::CFileWriteSecureArchive> SecureFileSerializer;
 
 
 } // namespace FilePck
