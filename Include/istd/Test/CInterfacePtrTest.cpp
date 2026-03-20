19d106beb52532047e8401248d84157b6d72e93c
Oleksii Maistrenko <oleksii.maistrenko@gmgcolor.com>
2026-03-20 22:10:53 +0100

    TInterfacePtr fix dynamicCast() on const

--- Include/istd/Test/CInterfacePtrTest.cpp
+++ Include/istd/Test/CInterfacePtrTest.cpp
@@ -90,11 +90,10 @@ void CInterfacePtrTest::DoSharedInterfaceTest()
 	}
 	// test dynamicCast() on const
 	{
-		// TODO:
-		// const istd::TSharedInterfacePtr<IInterface2> sharedPtrBase(new T22);
-		// auto sharedPtrDerived = sharedPtrBase.dynamicCast<IInterface22>();
-		// QVERIFY(sharedPtrDerived.IsValid());
-		// QVERIFY(sharedPtrDerived.GetBasePtr().use_count() == 2);
+		const istd::TSharedInterfacePtr<IInterface2> sharedPtrBase(new T22);
+		auto sharedPtrDerived = sharedPtrBase.dynamicCast<IInterface22>();
+		QVERIFY(sharedPtrDerived.IsValid());
+		QVERIFY(sharedPtrDerived.GetBasePtr().use_count() == 2);
 	}
 }
 
