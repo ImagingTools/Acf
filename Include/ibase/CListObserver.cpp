5a064ded37bc96b6669eb1bcb8b26dd0dae9d09e
Oleksii Maistrenko <oleksii.maistrenko@gmgcolor.com>
2026-04-08 19:04:45 +0200

    split CListObserver into observer and dispatcher helper
    the dispatcher can be used when the Observer is already implement by the other class, e.g. TSingleModelObserver

--- Include/ibase/CListObserver.cpp
+++ Include/ibase/CListObserver.cpp
@@ -5,12 +5,9 @@
 namespace ibase
 {
 
+// protected methods
 
-// public methods
-
-// reimplemented (imod::CSingleModelObserverBase)
-
-void CListObserver::OnUpdate(const istd::IChangeable::ChangeSet& changeSet)
+void CListObserverDispatcher::DispatchListChange(const istd::IChangeable::ChangeSet& changeSet)
 {
 	if (changeSet.Contains(CObservableListBase::CF_ELEMENT_ADDED)) {
 		OnAfterElementAdded(changeSet.GetChangeInfo(CObservableListBase::CN_INDEX_ID).toInt());
@@ -21,9 +18,19 @@ void CListObserver::OnUpdate(const istd::IChangeable::ChangeSet& changeSet)
 	if (changeSet.Contains(CObservableListBase::CF_ELEMENT_UPDATED)) {
 		OnAfterElementUpdated(changeSet.GetChangeInfo(CObservableListBase::CN_INDEX_ID).toInt());
 	}
-	if (changeSet.Contains(istd::IChangeable::CF_ALL_DATA) || changeSet.Contains(CObservableListBase::CF_RESET)) {
+	if (changeSet.Contains(istd::IChangeable::CF_ALL_DATA) || changeSet.Contains(CObservableListBase::CF_RESET) 
+		|| changeSet.Contains(istd::IChangeable::CF_NO_UNDO)) {
 		OnListReset();
 	}
+}
+
+// public methods
+
+// reimplemented (imod::CSingleModelObserverBase)
+
+void CListObserver::OnUpdate(const istd::IChangeable::ChangeSet& changeSet)
+{
+	CListObserverDispatcher::DispatchListChange(changeSet);
 }
 
 
