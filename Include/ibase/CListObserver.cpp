a81309db33caeeeac2882e0ebb1228eb4fbe61f7
Oleksii Maistrenko <oleksii.maistrenko@gmgcolor.com>
2026-04-10 10:13:35 +0200

    CListObserverDispatcher skip other changes on reset event

--- Include/ibase/CListObserver.cpp
+++ Include/ibase/CListObserver.cpp
@@ -9,6 +9,12 @@ namespace ibase
 
 void CListObserverDispatcher::DispatchListChange(const istd::IChangeable::ChangeSet& changeSet)
 {
+	if (changeSet.Contains(istd::IChangeable::CF_ALL_DATA) || changeSet.Contains(CObservableListBase::CF_RESET) 
+		|| changeSet.Contains(istd::IChangeable::CF_NO_UNDO)) {
+		OnListReset();
+		return; // do not process other changes if cumulative changeSet contains reset
+	}
+
 	if (changeSet.Contains(CObservableListBase::CF_ELEMENT_ADDED)) {
 		OnAfterElementAdded(changeSet.GetChangeInfo(CObservableListBase::CN_INDEX_ID).toInt());
 	}
@@ -17,10 +23,6 @@ void CListObserverDispatcher::DispatchListChange(const istd::IChangeable::Change
 	}
 	if (changeSet.Contains(CObservableListBase::CF_ELEMENT_UPDATED)) {
 		OnAfterElementUpdated(changeSet.GetChangeInfo(CObservableListBase::CN_INDEX_ID).toInt());
-	}
-	if (changeSet.Contains(istd::IChangeable::CF_ALL_DATA) || changeSet.Contains(CObservableListBase::CF_RESET) 
-		|| changeSet.Contains(istd::IChangeable::CF_NO_UNDO)) {
-		OnListReset();
 	}
 }
 
