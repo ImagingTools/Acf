1fcfba8be9352f940a8b3e83a956ddb7ada2ae74
Vladimir Cherepanov <ext.vladimir.cherepanov@gmgcolor.com>
2026-06-02 14:56:00 +0600

    CSubstractiveColorModel: Add notofocations

--- Include/ibase/CObservableListBase.h
+++ Include/ibase/CObservableListBase.h
@@ -24,15 +24,19 @@ public:
 		CF_ELEMENT_ADDED = 0x382b230,
 		CF_ELEMENT_REMOVED,
 		CF_ELEMENT_UPDATED,
+		CF_ELEMENT_MOVED,
 		CF_RESET
 	};
 
-	inline static const QByteArray CN_INDEX_ID = QByteArrayLiteral("INDEX");
+	inline static const QByteArray s_CN_INDEX_ID = QByteArrayLiteral("INDEX");
+	inline static const QByteArray s_CN_INDEX_FROM_ID = QByteArrayLiteral("INDEX_FROM");
+	inline static const QByteArray s_CN_INDEX_TO_ID = QByteArrayLiteral("INDEX_TO");
 
 protected:
 	static istd::IChangeable::ChangeSet ElementAddChanges(qsizetype index);
 	static istd::IChangeable::ChangeSet ElementRemoveChanges(qsizetype index);
 	static istd::IChangeable::ChangeSet ElementUpdatedChanges(qsizetype index);
+	static istd::IChangeable::ChangeSet ElementMoveChanges(qsizetype fromIndex, qsizetype toIndex);
 };
 
 
